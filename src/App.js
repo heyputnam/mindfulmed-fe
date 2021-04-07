5187af83f8fb70ca074c2d3ef953be083ebfaf34
heyputnam <heyputnam@gmail.com>
2021-04-07 13:03:40 -0400

    added header component and linked sylesheet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-
-import './App.css';
+//import statements
+import{ useState, useEffect } from 'react';
+import './styles.css';
+import Header from './components/Header/Header'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Header />
         <h1>MindfulMed</h1>
       </header>
     </div>
