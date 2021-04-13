1ce4fe7fb0cab1cfccdf05ec2a5fd3a0a3c604e0
heyputnam <heyputnam@gmail.com>
2021-04-13 17:56:54 -0400

    got calender to pop up with event in new window

--- src/components/GoogleCal/GoogleCal.js
+++ src/components/GoogleCal/GoogleCal.js
@@ -9,31 +9,67 @@ function GoogleCal(){
     var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
     var SCOPES = "https://www.googleapis.com/auth/calendar"
 
-    const handleClick= () =>{
-        gapi.load('client:auth2', () => {
-            console.log('loaded client')
+  const handleClick = () => {
+    gapi.load('client:auth2', () => {
+      console.log('loaded client')
 
+      gapi.client.init({
+        apiKey: API_KEY,
+        clientId: CLIENT_ID,
+        discoveryDocs: DISCOVERY_DOCS,
+        scope: SCOPES,
+      })
 
-        gapi.client.init({
-            apiKey: API_KEY,
-            clientId: CLIENT_ID,
-            discoveryDocs: DISCOVERY_DOCS,
-            scope: SCOPES,
+      gapi.client.load('calendar', 'v3', () => console.log('bam!'))
+
+      gapi.auth2.getAuthInstance().signIn()
+      .then(() => {
+        
+        var event = {
+          'summary': 'Awesome Event!',
+          'location': '800 Howard St., San Francisco, CA 94103',
+          'description': 'Really great refreshments',
+          'start': {
+            'dateTime': '2020-06-28T09:00:00-07:00',
+            'timeZone': 'America/Los_Angeles'
+          },
+          'end': {
+            'dateTime': '2020-06-28T17:00:00-07:00',
+            'timeZone': 'America/Los_Angeles'
+          },
+          'recurrence': [
+            'RRULE:FREQ=DAILY;COUNT=2'
+          ],
+          'attendees': [
+            {'email': 'lpage@example.com'},
+            {'email': 'sbrin@example.com'}
+          ],
+          'reminders': {
+            'useDefault': false,
+            'overrides': [
+              {'method': 'email', 'minutes': 24 * 60},
+              {'method': 'popup', 'minutes': 10}
+            ]
+          }
+        }
+
+        var request = gapi.client.calendar.events.insert({
+          'calendarId': 'primary',
+          'resource': event,
         })
-        gapi.client.load('calendar', 'v3', () => console.log('tadaa'))
 
-
-        gapi.auth2.getAuthInstance().signIn()
+        request.execute(event => {
+          console.log(event)
+          window.open(event.htmlLink)
         })
-    }
-    return(
-    <div>
-        <button onClick={handleClick}>pressme</button>
-
-    </div>
-    )
+  
+    })
+    })
 }
-
+return(
+    <div onClick={handleClick}>button</div>
+)
+}
 export default GoogleCal;
 
 
